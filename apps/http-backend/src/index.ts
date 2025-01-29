5cfa765d029816277d046104c0820cd32afdbbae
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-29 14:17:56 -0800

    hello

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
+const port = process.env.PORT || 3001;
 
 
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3001");
 });
